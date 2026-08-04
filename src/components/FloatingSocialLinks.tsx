9c12d8eda4bbe7a58f353924762df08424d8d4e5
badabuyindia-code <badabuyindia@gmail.com>
2026-08-04 22:47:38 +0530

    Update FloatingSocialLinks.tsx

--- src/components/FloatingSocialLinks.tsx
+++ src/components/FloatingSocialLinks.tsx
@@ -26,7 +26,18 @@ function ThreadsIcon({ className }: IconProps) {
   );
 }
 
+function WhatsAppIcon({ className }: IconProps) {
+  return (
+    <svg viewBox="0 0 24 24" fill="currentColor" className={className}>
+      <path d="M12.04 2C6.58 2 2.13 6.45 2.13 11.91c0 1.75.46 3.45 1.32 4.95L2 22l5.28-1.38a9.9 9.9 0 0 0 4.76 1.21h.01c5.46 0 9.9-4.45 9.9-9.92 0-2.65-1.03-5.14-2.9-7.01A9.87 9.87 0 0 0 12.04 2zm0 18.14h-.01a8.23 8.23 0 0 1-4.2-1.15l-.3-.18-3.13.82.84-3.05-.2-.31a8.22 8.22 0 0 1-1.26-4.36c0-4.54 3.7-8.24 8.26-8.24 2.2 0 4.28.86 5.84 2.42a8.19 8.19 0 0 1 2.42 5.83c0 4.55-3.7 8.24-8.26 8.24zm4.52-6.17c-.25-.12-1.47-.72-1.7-.81-.23-.08-.39-.12-.56.13-.17.25-.64.81-.78.97-.14.17-.29.19-.54.06-.25-.12-1.04-.38-1.98-1.22-.73-.65-1.23-1.46-1.37-1.7-.14-.25-.02-.38.11-.51.11-.11.25-.29.37-.43.12-.15.16-.25.25-.42.08-.17.04-.31-.02-.44-.06-.12-.56-1.34-.76-1.84-.2-.48-.41-.42-.56-.42-.14-.01-.31-.01-.48-.01a.92.92 0 0 0-.67.31c-.23.25-.87.85-.87 2.07 0 1.22.89 2.4 1.01 2.57.12.17 1.75 2.67 4.24 3.74.59.26 1.05.41 1.41.52.59.19 1.13.16 1.56.1.48-.07 1.47-.6 1.67-1.18.21-.58.21-1.08.15-1.18-.06-.1-.23-.16-.48-.28z" />
+    </svg>
+  );
+}
+
+const WHATSAPP_NUMBER = "918679573322";
+
 const SOCIAL_LINKS = [
+  { name: "WhatsApp", href: `https://wa.me/${WHATSAPP_NUMBER}`, Icon: WhatsAppIcon },
   { name: "Threads", href: "https://www.threads.com/@ekokintsugi", Icon: ThreadsIcon },
   { name: "Instagram", href: "https://www.instagram.com/ekokintsugi?igsh=ZTVtcjNkOW1mOWxz", Icon: InstagramIcon },
   { name: "Facebook", href: "https://www.facebook.com/share/19EEUHuaSh/", Icon: FacebookIcon }
